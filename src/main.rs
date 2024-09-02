6f104f5a63382a001933db57bc0d3c8dfa1312a9
Jaramogi <amolocaleb@gmail.com>
2024-09-02 18:24:02 +0300

    Weird Algorithm problem

---
+++ src/main.rs
@@ -0,0 +1,6 @@
+mod weird_algorithm;
+use crate::weird_algorithm::weird_algo;
+
+fn main() {
+    weird_algo(1)
+}
---
+++ src/weird_algorithm.rs
@@ -0,0 +1,26 @@
+
+pub fn weird_algo(input: u32) {
+    let mut num = input;
+    
+    // if num == 1 {
+    //     println!("{:?}",&num);
+    //     return;
+    // } 
+    let mut output: Vec<u32> = Vec::new();
+    output.push(*(&num));
+    let mask = 1 << 0;//even number test mask
+    
+    while num > 1 {
+        let even_odd = &num & mask;
+        if even_odd != 0 {
+            num = (num*3)+1;
+            output.push(*(&num)) 
+        } else {
+            num = (num)/2;
+            output.push(*(&num))
+        }
+    }
+
+    println!("{:?}",output.iter().map(|e|e.to_string()).collect::<Vec<_>>().join(" -> "));
+    
+}
